7f6ede3b3b90a7fca210ab7a81ac88dfdf837607
Simone Pulcini <simone.pulcini@gmail.com>
2014-11-02 15:57:18 +0100

    aggiunta chiamata servizio da endpoint e aggiunta eccezione servizio

---
+++ GdgCultFest/src/main/java/org/gdgrome/lab/gdgcultfest/exceptions/SPARQLServiceExcetpion.java
@@ -0,0 +1,30 @@
+package org.gdgrome.lab.gdgcultfest.exceptions;
+
+public class SPARQLServiceExcetpion extends Exception {
+
+	public SPARQLServiceExcetpion() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public SPARQLServiceExcetpion(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public SPARQLServiceExcetpion(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public SPARQLServiceExcetpion(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public SPARQLServiceExcetpion(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
