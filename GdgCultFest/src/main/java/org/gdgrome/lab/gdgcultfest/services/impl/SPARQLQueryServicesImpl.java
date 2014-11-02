2d996e35d65c092d4508e814ecd0eb3129fd8295
Simone Pulcini <simone.pulcini@gmail.com>
2014-11-02 15:06:57 +0100

    Implementazione servizio sparql

---
+++ GdgCultFest/src/main/java/org/gdgrome/lab/gdgcultfest/services/impl/SPARQLQueryServicesImpl.java
@@ -0,0 +1,19 @@
+package org.gdgrome.lab.gdgcultfest.services.impl;
+
+import org.gdgrome.lab.gdgcultfest.services.SPARQLQueryServices;
+
+public class SPARQLQueryServicesImpl implements SPARQLQueryServices {
+	
+	
+
+	public SPARQLQueryServicesImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String callSparqlEndpoint(String sparqlQuery, String limit) {
+	
+		return null;
+	}
+
+}
