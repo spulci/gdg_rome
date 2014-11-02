0eade5e0267657518f60a362f5a84f7d86cf3917
Simone Pulcini <simone.pulcini@gmail.com>
2014-11-02 22:45:02 +0100

    aggiunto cult pojo

---
+++ GdgCultFest/src/main/java/org/gdgrome/lab/gdgcultfest/beans/CultPojo.java
@@ -0,0 +1,23 @@
+package org.gdgrome.lab.gdgcultfest.beans;
+
+
+public class CultPojo {
+
+	private String queryResult;
+	
+	public CultPojo() {
+		// TODO Auto-generated constructor stub
+		
+	}
+
+	public String getQueryResult() {
+		return queryResult;
+	}
+
+	public void setQueryResult(String queryResult) {
+		this.queryResult = queryResult;
+	}
+	
+	
+
+}
