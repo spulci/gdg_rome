037c4a422a3cab26ba09a79a2c7ec8e7139f1494
Camelia Boban <info@aissatechnologies.com>
2014-11-02 15:27:15 +0100

    Aggiunto commento

--- GdgCultFest/src/main/java/org/gdgrome/lab/gdgcultfest/services/SPARQLQueryServices.java
+++ GdgCultFest/src/main/java/org/gdgrome/lab/gdgcultfest/services/SPARQLQueryServices.java
@@ -1,5 +1,7 @@
 package org.gdgrome.lab.gdgcultfest.services;
 
+
+//interfaccia endpoint
 public interface SPARQLQueryServices {
 
 	public final static String DBPEDIA_ENDPOINT = "http://dbpedia.org/sparql";
